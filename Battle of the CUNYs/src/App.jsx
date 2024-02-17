720a3d6b7e5bffb5eca83ee5ad7f3d6b0b8a57f5
darausz <darauszhang@gmail.com>
2024-02-17 00:35:43 -0500

    added Signup path

--- Battle of the CUNYs/src/App.jsx
+++ Battle of the CUNYs/src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { Routes, Route } from "react-router-dom"
 import Login from './routes/Login.jsx'
 import Authentication from './routes/Authentication.jsx'
 import Profile from './routes/Profile.jsx'
 import Home from './routes/Home.jsx'
+import Signup from './routes/Signup.jsx'
 import './App.css'
 
 function App() {
@@ -14,6 +12,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/Signup" element={<Signup />} />
         <Route
           path="home"
           element={
--- Battle of the CUNYs/src/routes/Login.jsx
+++ Battle of the CUNYs/src/routes/Login.jsx
@@ -1,55 +1,52 @@
 import React, { useState } from "react";
 import { useAuth } from "../provider/AuthProvider.jsx";
-import { useLocation, useNavigate } from "react-router-dom";
-import { Link } from "react";
+import { useLocation, useNavigate, Link } from "react-router-dom";
 export default function Login() {
-    const [user, setUser] = useState("");
-    const { login } = useAuth();
-    const navigate = useNavigate();
-    const location = useLocation();
-    const redirectPath = location.state?.path || "home";
+  const [user, setUser] = useState("");
+  const { login } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const redirectPath = location.state?.path || "home";
 
-    function handleLogin() {
-        login(user);
-    }
+  function handleLogin() {
+    login(user);
+  }
 
-    return (
-        <>
-            <section className="login">
-                <div className="flex-container">
-                    <h1 className="title">Login Account</h1>
-                    <label className="sub-text">Login Account</label>
-                    <input
-                        className="input"
-                        placeholder="Enter your email"
-                        type="text"
-                        onChange={(e) => {
-                            setUser(e.target.value);
-                        }}
-                    />
-                    <label className="sub-text">Password</label>
-                    <input
-                        className="input"
-                        placeholder="Enter your password"
-                        type="text"
-                        // onChange={(e) => {
-                        //     setUser(e.target.value);
-                        // }}
-                    />
-                    <button
-                        className="button"
-                        type="submit"
-                        onClick={handleLogin}
-                    >
-                        Log In
-                    </button>
-                    <p className="sub-caption url">
-                        <Link onClick={() => navigate("/signup")}>
-                            Don’t have an account? Sign up
-                        </Link>
-                    </p>
-                </div>
-            </section>
-        </>
-    );
+  return (
+    <>
+      <section className="login">
+        <div className="flex-container">
+          <h1 className="title">Login Account</h1>
+          <label className="sub-text">Login Account</label>
+          <input
+            className="input"
+            placeholder="Enter your email"
+            type="text"
+            onChange={(e) => {
+              setUser(e.target.value);
+            }}
+          />
+          <label className="sub-text">Password</label>
+          <input
+            className="input"
+            placeholder="Enter your password"
+            type="text"
+          // onChange={(e) => {
+          //     setUser(e.target.value);
+          // }}
+          />
+          <button
+            className="button"
+            type="submit"
+            onClick={handleLogin}
+          >
+            Log In
+          </button>
+          <Link to="/Signup" className="sub-caption url">
+            Don’t have an account? Sign up
+          </Link>
+        </div>
+      </section>
+    </>
+  );
 }
