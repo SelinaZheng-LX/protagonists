26b4fbf437d36f0d1b645f5aacbfd8f6ce97067a
SelinaZheng <selinazhenglx@gmail.com>
2024-02-17 01:51:47 -0500

    upper section of home design

--- Battle of the CUNYs/src/routes/Home.jsx
+++ Battle of the CUNYs/src/routes/Home.jsx
@@ -8,11 +8,31 @@ export default function Home() {
     const navigate = useNavigate();
     return (
         <>
-            Welcome {user.username}
-            <button
-                onClick={() => navigate("/profile")}>
-                mascot stats
-            </button>
+            <section className="homeScreen">
+                {/* <img src="../assets/Icon.png" alt="Protagonist's Logo" /> */}
+                <div className="col-container">
+                    <div className="upper-sec">
+                        <div className="row-container">
+                            <h2 className="h2">Quests</h2>
+                            <h2 className="h2">Profile</h2>
+                        </div>
+                        <h1 className="h1">Welcome, {user.username}</h1>
+                        <button
+                            className="button"
+                            onClick={() => navigate("/mascot")}
+                        >
+                            Feed me!
+                        </button>
+                        <div className="amount-fed">
+                            <p className="small-text">Amount fed today</p>
+                            <span className="placeholder-progress"></span>
+                        </div>
+                    </div>
+                    <button onClick={() => navigate("/profile")}>
+                        mascot stats
+                    </button>
+                </div>
+            </section>
         </>
-    )
+    );
 }
